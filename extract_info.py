4a8c7b1642464295869ff08eb554a42d2ee93b1c
pradyutnair <p.nair@student.tue.nl>
2022-12-04 16:13:12 +0100

    Inital commit

---
+++ extract_info.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+import string
+import tabula
+import warnings
+import regex as re
+
+warnings.filterwarnings("ignore")
+
+
+def grades_table_creator(file):
+    df = tabula.read_pdf(file, pages="3-5", multiple_tables=False, columns=[0.9, 2.2, 6.1, 7, 7.7, 9])
+    df = pd.DataFrame(df[0])
+    df = df.reset_index().T.reset_index().T
+    df = df.reset_index().iloc[:, 2:]
+    df.columns = ['course_type', 'course_code', 'course_name', 'credits', 'credits_duplicated', 'date', 'final_grades',
+                  'category']
+
+
+    def fix_credits(column):
+        column = column.astype("str")
+        pattern = r"([^\d|\.])"
+        creds_2 = []
+        for i in column:
+            i = re.sub(pattern, '', i)
+            if i == '' or float(i) < 5 or float(i) > (15):
+                i = 0
+            creds_2.append(float(i))
+        return creds_2
+
+
+    df.credits = fix_credits(df.credits)
+    df.credits_duplicated = fix_credits(df.credits_duplicated)
+
+    df.credits = df.credits + df.credits_duplicated
+    df = df.drop('credits_duplicated', axis=1)
+
+    dates = df.date.astype("str")
+    final_grades = df.final_grades.astype("str")
+
+    date_pattern = r"(\d{2}-\d{2}-\d{4})"
+    grade_pattern = r"(\s\d+\.\d)"
+    date_fixed = []
+    grades_fixed = []
+
+    for i in dates:
+        date = re.findall(date_pattern, i)
+        if not date:
+            date_fixed.append('No-Date')
+        else:
+            date_fixed.append(date[0])
+        grades_fixed.append(re.sub(date_pattern, '', i))
+
+    final_grades_fixed = []
+    for i in grades_fixed:
+        try:
+            if i == 'nan' or float(i) < 5:
+                final_grades_fixed.append(0)
+            else:
+                final_grades_fixed.append(float(i))
+
+
+
+        except:
+            final_grades_fixed.append(0)
+
+    df.final_grades = df.final_grades.replace(np.nan, 0)
+    df.final_grades = df.final_grades.astype("float") + np.array(final_grades_fixed)
+    df.date = date_fixed
+
+    course_pattern = r"((2|\w){4,}\d)"
+    df.course_code = df.course_code.astype("str")
+    course_codes = df.course_code
+    course_code_fixed = []
+    course_name_fixed = []
+    for i in course_codes:
+        code = re.findall(course_pattern, i)
+        if not code:
+            course_code_fixed.append('')
+        else:
+            course_code_fixed.append(code[0][0])
+        course_addition = re.sub(course_pattern, '', i)
+        if course_addition == 'nan':
+            course_name_fixed.append('')
+        else:
+            course_name_fixed.append(re.sub(course_pattern, '', i))
+
+    df.course_code = course_code_fixed
+    df.course_name = df.course_name.fillna('')
+    df.course_name = df.course_name + np.array(course_name_fixed)
+    return df[df.credits > 0]
+
---
+++ main.py
@@ -0,0 +1,98 @@
+import streamlit as st
+import pandas as pd
+from pytz import country_names
+from st_aggrid import AgGrid, GridUpdateMode, JsCode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+import os
+import sys
+from streamlit import cli as stcli
+import streamlit
+from extract_info import grades_table_creator
+
+
+def main():
+    @st.experimental_memo
+    def load_data():
+        df = pd.read_csv("grades.csv", encoding="latin-1")
+        return df
+
+    @st.experimental_memo
+    def convert_df(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv()
+
+    # The code below is for the title and logo.
+    st.set_page_config(page_title="GPA Calculator", page_icon="🔢", layout="wide")
+    st.image(
+        "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/closed-book_1f4d5.png",
+        width=100,
+    )
+    # conn = init_connection()
+
+    st.title("TU/e GPA Calculator")
+    st.write("")
+    st.markdown(
+        """Find it cumbersome to manually calculate your GPA from `Osiris'` poorly designed document? This tool does it 
+        for you automatically! Also, you can edit the cells containing your course & grade information to ensure your scores 
+        are accurate."""
+    )
+    st.write("")
+    uploaded_file = st.file_uploader('Upload your .pdf file', type="pdf")
+    if uploaded_file:
+        df = grades_table_creator(uploaded_file)
+
+        st.write("")
+        st.subheader("① Edit and select courses you want to include")
+        st.write("Osiris includes all the courses in your elective packages and you probably don't want the "
+                 "ones you haven't taken factoring into your GPA.")
+        st.info("💡 Hold the `Shift` (⇧) key to select multiple rows at once.")
+        st.caption("")
+        gd = GridOptionsBuilder.from_dataframe(df)
+        gd.configure_pagination(enabled=True)
+        gd.configure_default_column(editable=True, groupable=True)
+        gd.configure_selection(selection_mode="multiple", use_checkbox=True)
+        gridoptions = gd.build()
+        grid_table = AgGrid(
+            df,
+            gridOptions=gridoptions,
+            update_mode=GridUpdateMode.SELECTION_CHANGED,
+            theme="material",
+        )
+        sel_row = grid_table["selected_rows"]
+
+        st.subheader(" ② Check your selection")
+
+        st.write("")
+
+        df_sel_row = pd.DataFrame(sel_row).iloc[:,1:]
+        csv = convert_df(df_sel_row)
+        if not df_sel_row.empty:
+            st.write(df_sel_row)
+            st.download_button(
+                label="Download to CSV",
+                data=csv,
+                file_name="results.csv",
+                mime="text/csv",
+            )
+            st.write("")
+            st.subheader("③ Your GPA")
+            gpa = str(round(df_sel_row["final_grades"].astype("float").mean(),1))
+            st.subheader(gpa)
+
+            st.write("")
+            st.subheader("④ Grade Progression")
+            data = df_sel_row.sort_values(by="date", ascending=True)[['course_name', 'final_grades']]
+            st.bar_chart(data=data,width=1000, height=700, use_container_width=True)
+
+    else:
+        st.write("Please upload your .pdf file to get started!")
+
+
+# Your streamlit code
+
+if __name__ == '__main__':
+    if streamlit._is_running_with_streamlit:
+        main()
+    else:
+        sys.argv = ["streamlit", "run", sys.argv[0]]
+        sys.exit(stcli.main())
